477ffc217f5e804a7c9291287e398c088e772714
Liguang Zhang <leezhang97@gmail.com>
2023-03-24 16:13:59 -0400

    adding existing files

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
-// We import useState and useEffect in our component
-import React, { useState, useEffect } from 'react';
-import SearchBar from './components/SearchBar';
-import IssueList from './components/IssueList';
+import React, { useEffect } from 'react';
+import { StudentProvider } from './utils/StudentContext';
+import StudentList from './components/StudentList';
+import './app.css';
 
 function App() {
-  // We declare a state variable that is an array called `issues` and a function to update it.
-  const [issues, setIssues] = useState([]);
-
-  // When the page loads, set the document title to something specific to this app.
-  // This only runs once because of our empty dependency array.
+  const title = 'Activity 20: useReducer Review';
   useEffect(() => {
-    document.title = 'GitHub issues';
+    document.title = title;
   }, []);
 
-  // Helper function that performs an API request and sets the `issues` array to a list of issues from GitHub
-  const getRepoIssues = (repo) => {
-    let issuesURL = `https://api.github.com/repos/${repo}/issues?direction=asc`;
-    console.log('issuesURL', issuesURL);
-    fetch(issuesURL)
-      .then((res) => res.json())
-      .then((response) => setIssues(response));
-  };
-
   return (
-    <div className="ui container">
-      {/* Here we pass our getRepoIssues function as a prop to SearchBar */}
-      <SearchBar onFormSubmit={getRepoIssues} />
-      <div className="ui grid">
-        <div className="ui row">
-          <div className="eleven wide column">
-            <IssueList issues={issues} />
-          </div>
-        </div>
-      </div>
+    <div className="app">
+      <h1>22.1 State</h1>
+      <h4 style={{ color: 'lightseagreen' }}>{title}</h4>
+      {/* Provider wraps all the logic that handles/updates our state */}
+      <StudentProvider>
+        <StudentList />
+        
+      </StudentProvider>
     </div>
   );
 }
