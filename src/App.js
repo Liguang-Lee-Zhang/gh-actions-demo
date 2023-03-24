b141cd3625115efd8da129e8dceaa7f9fa51cab0
Liguang Zhang <leezhang97@gmail.com>
2023-03-24 16:33:49 -0400

    rid of App const

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { StudentProvider } from './utils/StudentContext';
 import StudentList from './components/StudentList';
 import './app.css';
 
-const App = 12
 function App() {
   const title = 'Activity 20: useReducer Review';
   useEffect(() => {
